880e113a5e7dfdd8b87010827b04dec4bac7eacb
mking01 <59142310+mking01@users.noreply.github.com>
2020-03-09 14:34:50 -0500

    Update Patient.py

--- src/lib/Patient.py
+++ src/lib/Patient.py
@@ -2,6 +2,29 @@
 
 class Patient:
     '''
-
+    id (int): patient identifier, unique
+    sex (string): patient gender
+    birth_year (year): year patient was born
+    region (string): region patient is from
+    group (string): additional affiliation of patient
+    infection_reason (string): how patient became infected
+    infection_order (int): reason 1-5 the patient was infected
+    contact_number (int)
+    confirmed_date (date): date confirmed
+    released_date (date): date released
+    deceased_date (date): date passed away
+    state (string): outcome - released, isolated, deceased
     '''
-    def __init__(self):
+    def __init__(self, id, sex, birth_year, region, group, infection_reason, infection_order, contact_number,
+                 released_date, deceased_date, state):
+        self.id = id
+        self.sex = sex
+        self.birth_year = birth_year
+        self.region = region
+        self.group = group
+        self.infection_reason = infection_reason
+        self.infection_order = infection_order
+        self.contact_number = contact_number
+        self.released_date = released_date
+        self.deceased_date = deceased_date
+        self.state = state
