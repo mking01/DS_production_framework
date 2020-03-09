f4f4527626deed37b37d7f66868f10b63893873e
mking01 <59142310+mking01@users.noreply.github.com>
2020-03-09 14:34:38 -0500

    Create configs.py

---
+++ src/configs/configs.py
@@ -0,0 +1,8 @@
+# Configs for frequently used and potentially changing parts of the project
+path = "/Users/mollyking/Documents/Github/DS_production_framework/data/"
+
+patient_file = 'patient.csv'
+route_file = 'route.csv'
+time_file = 'time.csv'
+trend_file = 'trend.csv'
+
