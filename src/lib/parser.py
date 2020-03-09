0abd4398be12ddb79905f1a10b45d8c980aa399d
mking01 <59142310+mking01@users.noreply.github.com>
2020-03-09 14:34:48 -0500

    Update parser.py

--- src/lib/parser.py
+++ src/lib/parser.py
@@ -2,10 +2,38 @@
 
 import sys
 import pandas as pd
-#from Patient import Patient
-import os
+from configs import path, patient_file#, route_file, time_file, trend_file
+from Patient import Patient
 
-path = os.getcwd()
-print(path)
+patients_df = pd.read_csv(path + patient_file)
+print(patients_df.head())
 
-#path = sys.argv[1]
+# read patient rows into patient class
+for index, row in patients_df.iterrows():
+
+    # create empty list to hold all patients
+    patients = []
+
+    # match file features to class attributes
+    id = patients_df['id'].iloc[index]
+    sex = patients_df['sex'].iloc[index]
+    birth_year = patients_df['birth_year'].iloc[index]
+    region = patients_df['region'].iloc[index]
+    group = patients_df['group'].iloc[index]
+    infection_reason = patients_df['infection_reason'].iloc[index]
+    infection_order = patients_df['infection_order'].iloc[index]
+    contact_number = patients_df['contact_number'].iloc[index]
+    released_date = patients_df['released_date'].iloc[index]
+    deceased_date = patients_df['deceased_date'].iloc[index]
+    state = patients_df['state'].iloc[index]
+
+    # create a patient object, pass features to attributes
+    patient = Patient(id=id, sex=sex, birth_year=birth_year, region=region, group=group,
+                       infection_reason=infection_reason, infection_order=infection_order,
+                       contact_number=contact_number, released_date=released_date, deceased_date=deceased_date,
+                       state=state)
+
+    # append patient to list of patients
+    patients.append(patient)
+
+print(patients)
